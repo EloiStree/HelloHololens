eb2028f5839a24fd7cdb85ae940ffc4cc9b436c5
Eloi Stree <jams.center@gmail.com>
2019-02-28 14:56:40 +0100

    Create a spaceship that follow the camera view

---
+++ Move Your Ass/Assets/FakeCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeCamera : MonoBehaviour
+{
+    public Transform m_affectedTransform;
+    public float m_horizontalRotation = 40f;
+    public float m_verticalRotation = 40f;
+    public float m_currentRotationHorizontal;
+    public float m_currentRotationVertical;
+
+    void Update()
+    {
+        Vector2 rotateCameraOf = new Vector2(Input.GetAxis("Horizontal")*m_horizontalRotation, Input.GetAxis("Vertical") * m_verticalRotation);
+        m_currentRotationHorizontal += rotateCameraOf.x * Time.deltaTime;
+        m_currentRotationVertical += rotateCameraOf.y * Time.deltaTime;
+        m_affectedTransform.localRotation =Quaternion.Euler( new Vector3(-m_currentRotationVertical, m_currentRotationHorizontal, 0));
+
+
+    }
+    public void Reset()
+    {
+        m_affectedTransform = this.transform;
+    }
+}
---
+++ Move Your Ass/Assets/SpaceShipPosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipPosition : MonoBehaviour
+{
+    public Transform m_affectedSpaceShip;
+    public Transform m_cameraDirection;
+    public Transform m_wantedPosition;
+
+
+    void Update()
+    {
+
+        Vector3 cameraDirection = m_cameraDirection.forward;
+        cameraDirection.y = 0;
+        cameraDirection.x = 0;
+        m_affectedSpaceShip.forward = Vector3.Lerp(m_affectedSpaceShip.forward, cameraDirection, Time.deltaTime);
+        m_affectedSpaceShip.position = Vector3.Lerp(m_affectedSpaceShip.position, m_wantedPosition.position, Time.deltaTime);
+
+        
+    }
+}
