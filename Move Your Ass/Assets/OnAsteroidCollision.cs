9e4b714ae9ac3cea80188132e30930ba1067ec51
Eloi Stree <jams.center@gmail.com>
2019-02-28 15:49:54 +0100

    Add possibility for spaceship to die rom asteroid

---
+++ Move Your Ass/Assets/OnAsteroidCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnAsteroidCollision : MonoBehaviour
+{
+    public OnAsteroidHitEvent m_collisionWithAsteroid;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+     AsteroidTag  asteroid =   collision.rigidbody.gameObject.GetComponent<AsteroidTag>();
+        m_collisionWithAsteroid.Invoke(asteroid,collision);
+    }
+
+    [System.Serializable]
+    public class OnAsteroidHitEvent: UnityEvent<AsteroidTag, Collision> { }
+}
---
+++ Move Your Ass/Assets/SpaceShipDestruction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipDestruction : MonoBehaviour
+{
+    public GameObject m_explosion;
+    public Transform m_explosionPosition;
+    public GameObject m_spaceShipToDestroy;
+    
+    public void BigBadaBoum()
+    {
+        GameObject.Instantiate(m_explosion, m_explosionPosition.position, m_explosionPosition.rotation);
+        m_spaceShipToDestroy.SetActive(false);
+    }
+}
