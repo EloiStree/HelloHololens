dcc24753cb2bd1c035e9194dfef11f3a2b72abf8
Eloi Stree <jams.center@gmail.com>
2019-03-03 17:38:59 +0100

    Stable version with SceneLoading MR and classic

--- Move Your Ass/Assets/MoveYourAss/Script/FollowMainCamera.cs
+++ Move Your Ass/Assets/MoveYourAss/Script/FollowMainCamera.cs
@@ -8,12 +8,19 @@ public class FollowMainCamera : MonoBehaviour
     public Transform m_affected;
     void Update()
     {
-        m_affected.position = m_camera.position;
-        m_affected.rotation = m_camera.rotation;
+        if (m_camera == null && Camera.main != null )
+        {
+
+            m_camera = Camera.main.transform;
+        }
+        if (m_camera != null) {
+            m_affected.position = m_camera.position;
+            m_affected.rotation = m_camera.rotation;
+
+        }
     }
     private void Reset()
     {
-        m_camera = Camera.main.transform;
         m_affected = this.transform;
     }
 }
--- Move Your Ass/Assets/MoveYourAss/Script/SpaceShipPosition.cs
+++ Move Your Ass/Assets/MoveYourAss/Script/SpaceShipPosition.cs
@@ -8,16 +8,43 @@ public class SpaceShipPosition : MonoBehaviour
     public Transform m_cameraDirection;
     public Transform m_wantedPosition;
 
+    private void Start()
+    {
+
+        TryToFindPositionToFollow();
+    }
 
     void Update()
     {
 
-        Vector3 cameraDirection = m_cameraDirection.forward;
-        cameraDirection.y = 0;
-        cameraDirection.x = 0;
-        m_affectedSpaceShip.forward = Vector3.Lerp(m_affectedSpaceShip.forward, cameraDirection, Time.deltaTime);
-        m_affectedSpaceShip.position = Vector3.Lerp(m_affectedSpaceShip.position, m_wantedPosition.position, Time.deltaTime);
+        if (m_cameraDirection && m_wantedPosition) {
+            Vector3 cameraDirection = m_cameraDirection.forward;
+            cameraDirection.y = 0;
+            cameraDirection.x = 0;
+            m_affectedSpaceShip.forward = Vector3.Lerp(m_affectedSpaceShip.forward, cameraDirection, Time.deltaTime);
+            m_affectedSpaceShip.position = Vector3.Lerp(m_affectedSpaceShip.position, m_wantedPosition.position, Time.deltaTime);
 
+
+
+        }
         
+    }
+    private void Reset()
+    {
+        m_affectedSpaceShip = this.transform;
+        TryToFindPositionToFollow();
+    }
+  
+
+    private void TryToFindPositionToFollow()
+    {
+        SpaceShipPositionTag tag = GameObject.FindObjectOfType<SpaceShipPositionTag>();
+        if (tag)
+        {
+            if (m_cameraDirection == null)
+                m_cameraDirection = tag.transform;
+            if (m_wantedPosition == null)
+                m_wantedPosition = tag.transform;
+        }
     }
 }
